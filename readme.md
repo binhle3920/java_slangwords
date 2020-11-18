11c89791d3f0de2d1440d478a9d4dbccb447e1e7
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2020-11-18 08:36:27 +0700

    Read and structure data

---
+++ readme.md
@@ -0,0 +1,12 @@
+#Slang words project
+##User interface
+**UI is not important**, this is an console application (if have time, we will make UI)
+
+##Collections framework
+We will use [LinkedHashMap](https://www.javatpoint.com/java-linkedhashmap) to construct the dictionary.
+
+##Data
+Data is provided by Mr.Ho Tuan Thanh included *7662 words*
+
+##Duration
+This project will be completed in 20 days, from 13/11/2020 --> 3/12/2020
---
+++ slang_words/src/main/java/SlangWords.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author binh3920
+ */
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+//import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.ArrayList;
+        
+public class SlangWords {
+    public static void main(String args[]) {
+        //LinkedHashMap<String, List<String>> dictionary = new LinkedHashMap<String, List<String>>();
+        try {
+            FileInputStream fin = new FileInputStream("../slang.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fin));
+            
+            while(reader.ready()) {
+                String line = reader.readLine();
+                String slang = getSlang(line);
+                List<String> words = new ArrayList<String>();
+                words = getWords(line);
+                //dictionary.put(slang, words);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(SlangWords.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //System.out.println(dictionary);
+    }
+    private static String getSlang(String line) {
+        int pos = line.indexOf("`");
+        return line.substring(0, pos);
+    }
+    private static List<String> getWords(String line) {
+        int pos = line.indexOf("`");
+        String words = line.substring(pos+1, line.length());
+        List<String> listWords = new ArrayList<String>();
+        
+        while(true) {
+            pos = words.indexOf("|");
+            if (pos == -1) {
+                listWords.add(words);
+                return listWords;
+            } else {
+                listWords.add(words.substring(0, pos));
+                words = words.substring(pos+2, words.length());
+            }
+            
+        }
+    }
+    
+}
